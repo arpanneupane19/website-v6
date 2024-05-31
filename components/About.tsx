92a8e0165c9cc9199278d0ebbc14267af0069058
Arpan Neupane <arpanneupane19@gmail.com>
2024-05-31 15:46:40 -0500

    updated grade

--- components/About.tsx
+++ components/About.tsx
@@ -19,7 +19,7 @@ function About() {
           </h2>
           <p className="lg:mb-12 md:mb-8 mb-4 lg:text-xl md:text-lg text-base font-light">
             {" "}
-            I am a {age}-year-old programmer in my sophomore year of high school
+            I am a {age}-year-old programmer in my junior year of high school
             with a passion for software engineering. I enjoy building meaningful
             products that can express my creativity through code.{" "}
           </p>
